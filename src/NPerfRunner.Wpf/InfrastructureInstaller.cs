4a10f3a5092a27b5d6db5903797f4d277a6aef35
Maxim <fostreak@gmail.com>
2013-02-14 10:04:41 +0400

    Solved problem with ReactiveUI

--- src/NPerfRunner.Wpf/InfrastructureInstaller.cs
+++ src/NPerfRunner.Wpf/InfrastructureInstaller.cs
@@ -15,13 +15,32 @@ namespace NPerfRunner.Wpf
     {
         public static void Install()
         {
-            
+
 
             IoC.Instance.RegisterSingleton(typeof(IScreen), typeof(MainWindowViewModel));
             IoC.Instance.RegisterSingleton(typeof(ISettingsViewModel), typeof(SettingsViewModel));
             IoC.Instance.RegisterSingleton(typeof(IViewFor<ISettingsViewModel>), typeof(SettingsView));
 
             RxApp.DeferredScheduler = new DispatcherScheduler(Application.Current.Dispatcher);
+
+            RxRouting.ViewModelToViewFunc = str =>
+               {
+                   var viewModelType = AppDomain.CurrentDomain
+                       .GetAssemblies().SelectMany(x => x.GetTypes())
+                       .Where(type => type.AssemblyQualifiedName.Equals(str)).FirstOrDefault();
+
+                   if (viewModelType == null)
+                   {
+                       return string.Empty;
+                   }
+
+                   var interfaceName = string.Format("I{0}", viewModelType.Name);
+                   var viewModelInterface = viewModelType.GetInterface(interfaceName);
+
+                   var viewType = typeof(IViewFor<>).MakeGenericType(viewModelInterface);
+
+                   return viewType.AssemblyQualifiedName;
+               };
 
             RxApp.ConfigureServiceLocator(
                 (iface, contract) => IoC.Instance.Resolve(iface, contract),
