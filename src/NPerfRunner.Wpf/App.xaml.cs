6d6aead20fc60bc84752028924d87f21a3557ad9
Orcomp <Orcomp@server>
2014-01-14 15:04:03 +1100

    (*) Update target framework to .NET 4.5

--- src/NPerfRunner.Wpf/App.xaml.cs
+++ src/NPerfRunner.Wpf/App.xaml.cs
@@ -16,7 +16,6 @@
     /// </summary>
     public partial class App : Application
     {
-
         protected override void OnStartup(StartupEventArgs e)
         {            
             InfrastructureInstaller.Install();
